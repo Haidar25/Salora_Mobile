086a00cf73da08a5c3c6f7b685a8302b6e30552b
haidar <haidaralvin@gmail.com>
2019-11-08 09:52:01 +0700

    pertama

---
+++ app/src/main/java/com/EAI/Tubes/Global/Fungsi.java
@@ -0,0 +1,101 @@
+package com.EAI.Tubes.Global;
+
+import android.annotation.SuppressLint;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+
+import com.EAI.Tubes.R;
+
+public class Fungsi {
+    @SuppressLint("ResourceAsColor")
+    public static void pesan_sukses(View view, String pesan){
+//        Snackbar.make(view, pesan, Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show();
+
+        Snackbar snackbar = Snackbar.make(view, pesan, Snackbar.LENGTH_LONG)
+                .setAction("Action", null);
+        View sbView = snackbar.getView();
+        sbView.setBackgroundColor(R.color.sukses);
+        snackbar.show();
+    }
+
+    @SuppressLint("ResourceAsColor")
+    public static void pesan_error(View view, String pesan){
+//        Snackbar.make(view, pesan, Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show();
+
+        Snackbar snackbar = Snackbar.make(view, pesan, Snackbar.LENGTH_LONG)
+                .setAction("Action", null);
+        View sbView = snackbar.getView();
+        sbView.setBackgroundColor(R.color.error);
+        snackbar.show();
+
+
+    }
+
+    @SuppressLint("ResourceAsColor")
+    public static void pesan_warning(View view, String pesan){
+//        Snackbar.make(view, pesan, Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show();
+
+        Snackbar snackbar = Snackbar.make(view, pesan, Snackbar.LENGTH_LONG)
+                .setAction("Action", null);
+        View sbView = snackbar.getView();
+        sbView.setBackgroundColor(R.color.warning);
+        snackbar.show();
+    }
+
+    public String getJenislapang(int value){
+    String hasil;
+
+        switch(value) {
+            case 1:
+                hasil = "Sintetis";
+                break;
+            case 2:
+                hasil = "V-Sport";
+                break;
+            default:
+                hasil = "ERROR";
+        }
+
+        return hasil;
+    }
+
+    public String getStatusBooking(int value){
+        String hasil;
+
+        switch(value) {
+            case 1:
+                hasil = "Pending";
+                break;
+            case 2:
+                hasil = "Konfirmasi";
+                break;
+            case 3:
+                hasil = "Selesai";
+                break;
+            case 4:
+                hasil = "Cancel";
+                break;
+            default:
+                hasil = "ERROR";
+        }
+
+        return hasil;
+    }
+
+//    public int getRupiah(int value) {
+//        DecimalFormat kursIndonesia = (DecimalFormat) DecimalFormat.getCurrencyInstance();
+//        DecimalFormatSymbols formatRp = new DecimalFormatSymbols();
+//
+//        formatRp.setCurrencySymbol("Rp. ");
+//        formatRp.setMonetaryDecimalSeparator(',');
+//        formatRp.setGroupingSeparator('.');
+//
+//        kursIndonesia.setDecimalFormatSymbols(formatRp);
+//        int hasil =  kursIndonesia.format(value);
+//        return hasil;
+//    }
+
+}
---
+++ app/src/main/java/com/EAI/Tubes/HistoryBooking.java
@@ -0,0 +1,110 @@
+package com.EAI.Tubes;
+
+import android.app.ProgressDialog;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.EAI.Tubes.adapter.Koneksi;
+import com.EAI.Tubes.adapter.RecyclerViewAdapter;
+import com.EAI.Tubes.model.Booking;
+import com.EAI.Tubes.Global.Fungsi;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class HistoryBooking extends AppCompatActivity {
+    Koneksi kon = new Koneksi();
+    private final String JSON_URL = kon.ip()+"api/historybooking";
+    private JsonArrayRequest request;
+    private RequestQueue requestQueue;
+    private List<Booking> lstBooking;
+    private RecyclerView recyclerView;
+
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
+    Fungsi f =new Fungsi();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history_booking);
+
+        lstBooking = new ArrayList<>();
+
+        sharedPreferences = getSharedPreferences("loginmember", MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+
+        recyclerView = findViewById(R.id.recyclerviewid);
+        jsonrequest();
+    }
+
+    private void jsonrequest() {
+        final ProgressDialog dialog = ProgressDialog.show(this, null, "Please Wait");
+
+        request = new JsonArrayRequest(JSON_URL, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                dialog.dismiss();
+                JSONObject jsonObject = null;
+                for(int i = 0 ; i < response.length(); i++){
+
+                    try {
+
+                        jsonObject = response.getJSONObject(i);
+                        if (jsonObject.getInt("id") == sharedPreferences.getInt("id", 0)){
+                            Booking bo= new Booking();
+                            bo.setName(jsonObject.getString("name"));
+                            bo.setNama_lapang(jsonObject.getString("nama_lapang"));
+                            bo.setJenis_lapang(f.getJenislapang(jsonObject.getInt("jenis_lapang")));
+                            bo.setTgl_booking(jsonObject.getString("tgl_booking"));
+                            bo.setWaktu_booking(jsonObject.getString("waktu_booking"));
+                            bo.setHarga_lapang(jsonObject.getInt("harga_lapang"));
+                            bo.setStatus_booking(f.getStatusBooking(jsonObject.getInt("status_booking")));
+                            bo.setGambar_lapang(jsonObject.getString("gambar_lapang"));
+
+                            lstBooking.add(bo);
+                        }
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        Toast.makeText(HistoryBooking.this, "Ada yg gagal", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                setuprecyclerview(lstBooking);
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                dialog.dismiss();
+            }
+        });
+
+        requestQueue = Volley.newRequestQueue(HistoryBooking    .this);
+        requestQueue.add(request);
+    }
+
+    private void setuprecyclerview(List<Booking> lstBooking){
+        RecyclerViewAdapter myadapter = new RecyclerViewAdapter(this,lstBooking);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        recyclerView.setAdapter(myadapter);
+    }
+}
